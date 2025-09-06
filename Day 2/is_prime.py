562379aed722a9bc92bcf5db595dcde6e27bf789
Gayathri-0510 <gayathripadakanti@gmail.com>
2025-09-06 19:15:45 +0530

    Update and rename prime.py to is_prime.py

---
+++ Day 2/is_prime.py
@@ -0,0 +1,9 @@
+def is_prime(n):
+    for i in range (2,n):
+        if(n%i == 0):
+            print(n," is not a prime number")
+            break
+    else:
+        print(n," is a prime number")
+n=int(input("Enter a number: "))
+is_prime(n)
